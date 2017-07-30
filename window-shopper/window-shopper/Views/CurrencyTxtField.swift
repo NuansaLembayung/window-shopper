c7f97e69412a9da38b7d6a50021f19fbf7bad7bf
Nuansa Lembayung <nuansa@icloud.com>
2017-07-30 11:25:44 +0800

    add calculate button

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customiseView()
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        customiseView()
+    }
+    
+    func customiseView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2500550176)
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         layer.cornerRadius = 5.0
         textAlignment = .center
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
 }
